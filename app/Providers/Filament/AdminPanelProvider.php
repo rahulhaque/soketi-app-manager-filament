f69d213ddcdf6cfc581d8f1cfd1e22dcd9bdb11e
Rahul <rahulhaque07@gmail.com>
2023-08-30 16:30:01 +0600

    Application logo added

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->profile()
+            ->topNavigation()
             ->colors([
                 'primary' => Color::Violet,
             ])
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex shrink-0 items-center">
+    <img src="{{ asset('logo.png') }}" alt="soketi_logo" class="h-10"/>
+    <p class="text-xl font-bold text-gray-950 dark:text-white">{{ config('app.name') }}</p>
+</div>
