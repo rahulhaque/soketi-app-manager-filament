47a16fda2752f44dc951fd9abe70ceaae0922294
rahulhaque <rahulhaque07@gmail.com>
2025-10-08 21:34:45 +0600

    Trusted proxy configuration added ✨ (#16)
    * Trusted proxy configuration added ✨
    
    * Dependencies updated 📦

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
+
         Model::unguard();
 
         Blueprint::mixin(new BluePrintMixins);
